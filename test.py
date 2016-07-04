a0930519faa086ba4fb35a50487defae22de2eb5
hzh_mlx <hzh@melexis.com>
2016-07-04 16:09:41 +0800

    init file

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def enroll(name, gender, age = 6, city = 'Beijing'):
+    print('name:',name)
+    print('gender:',gender)
+    print('age:',age)
+    print('city:',city)
+
+enroll('Sarah', 'F')
+enroll('Sarah', 'F', age = 7)
+enroll('bob','M', city = 'Shanghai')
